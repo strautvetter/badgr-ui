0bd97419ab8b93e2c128cae3ddd24fe4f4d6027f
PaulaScharf <paulascharf@gmx.de>
2023-06-12 11:32:14 +0200

    if not logged in no user + reload user after login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,43 +1,44 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
-import {FormBuilder, Validators} from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
-import {EmailValidator} from '../../../common/validators/email.validator';
-import {UserCredential} from '../../../common/model/user-credential.type';
-import {SessionService} from '../../../common/services/session.service';
-import {MessageService} from '../../../common/services/message.service';
-import {BaseRoutableComponent} from '../../../common/pages/base-routable.component';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmailValidator } from '../../../common/validators/email.validator';
+import { UserCredential } from '../../../common/model/user-credential.type';
+import { SessionService } from '../../../common/services/session.service';
+import { MessageService } from '../../../common/services/message.service';
+import { BaseRoutableComponent } from '../../../common/pages/base-routable.component';
 import { DomSanitizer, Title } from '@angular/platform-browser';
-import {FormFieldText} from '../../../common/components/formfield-text';
+import { FormFieldText } from '../../../common/components/formfield-text';
 
-import {QueryParametersService} from '../../../common/services/query-parameters.service';
-import {OAuthManager} from '../../../common/services/oauth-manager.service';
-import {ExternalToolsManager} from '../../../externaltools/services/externaltools-manager.service';
-import {UserProfileManager} from '../../../common/services/user-profile-manager.service';
-import {HttpErrorResponse} from '@angular/common/http';
-import {AppConfigService} from '../../../common/app-config.service';
-import {typedFormGroup} from '../../../common/util/typed-forms';
+import { QueryParametersService } from '../../../common/services/query-parameters.service';
+import { OAuthManager } from '../../../common/services/oauth-manager.service';
+import { ExternalToolsManager } from '../../../externaltools/services/externaltools-manager.service';
+import { UserProfileManager } from '../../../common/services/user-profile-manager.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AppConfigService } from '../../../common/app-config.service';
+import { typedFormGroup } from '../../../common/util/typed-forms';
 import { BadgrApiFailure } from '../../../common/services/api-failure';
-
 
 @Component({
 	selector: 'login',
 	templateUrl: './login.component.html',
 })
 export class LoginComponent extends BaseRoutableComponent implements OnInit, AfterViewInit {
-
-	get theme() { return this.configService.theme; }
-	get features() { return this.configService.featuresConfig; }
+	get theme() {
+		return this.configService.theme;
+	}
+	get features() {
+		return this.configService.featuresConfig;
+	}
 	loginForm = typedFormGroup()
-		.addControl("username", "", [ Validators.required, EmailValidator.validEmail ])
-		.addControl("password", "", Validators.required)
-		.addControl("rememberMe", false)
-	;
+		.addControl('username', '', [Validators.required, EmailValidator.validEmail])
+		.addControl('password', '', Validators.required)
+		.addControl('rememberMe', false);
 
 	verifiedName: string;
 	verifiedEmail: string;
 
-	@ViewChild("passwordField", {static: false})
+	@ViewChild('passwordField', { static: false })
 	passwordField: FormFieldText;
 
 	initFinished: Promise<unknown> = new Promise(() => {});
@@ -58,11 +59,11 @@ export class LoginComponent extends BaseRoutableComponent implements OnInit, Aft
 		route: ActivatedRoute
 	) {
 		super(router, route);
-		title.setTitle(`Login - ${this.configService.theme['serviceName'] || "Badgr"}`);
+		title.setTitle(`Login - ${this.configService.theme['serviceName'] || 'Badgr'}`);
 		this.handleQueryParamCases();
 	}
 
-	sanitize(url:string){
+	sanitize(url: string) {
 		return this.sanitizer.bypassSecurityTrustUrl(url);
 	}
 
@@ -74,7 +75,6 @@ export class LoginComponent extends BaseRoutableComponent implements OnInit, Aft
 		if (this.verifiedEmail) {
 			this.loginForm.controls.username.setValue(this.verifiedEmail);
 		}
-
 	}
 
 	ngAfterViewInit(): void {
@@ -84,94 +84,117 @@ export class LoginComponent extends BaseRoutableComponent implements OnInit, Aft
 	}
 
 	submitAuth() {
-		if (! this.loginForm.markTreeDirtyAndValidate()) {
+		if (!this.loginForm.markTreeDirtyAndValidate()) {
 			return;
 		}
 
 		const credential: UserCredential = new UserCredential(
-			this.loginForm.value.username, this.loginForm.value.password);
+			this.loginForm.value.username,
+			this.loginForm.value.password
+		);
 
-		this.loginFinished = this.sessionService.login(credential)
+		this.loginFinished = this.sessionService
+			.login(credential)
 			.then(
 				() => {
-					this.profileManager.userProfilePromise.then((profile) => {
-						// fetch user profile and emails to check if they are verified
-						profile.emails.updateList().then(() => {
-							if (profile.isVerified) {
-								if (this.oAuthManager.isAuthorizationInProgress) {
-									this.router.navigate([ '/auth/oauth2/authorize' ]);
-								} else {
-									this.externalToolsManager.externaltoolsList.updateIfLoaded();
-									// catch localStorage.redirectUri
-									if (localStorage.redirectUri) {
-										const redirectUri = new URL(localStorage.redirectUri);
-										localStorage.removeItem('redirectUri');
-										window.location.replace(redirectUri.origin);
-										return false;
+					this.profileManager.reloadUserProfileSet().then(() => {
+						this.profileManager.userProfilePromise.then((profile) => {
+							if (profile) {
+								// fetch user profile and emails to check if they are verified
+								profile.emails.updateList().then(() => {
+									if (profile.isVerified) {
+										if (this.oAuthManager.isAuthorizationInProgress) {
+											this.router.navigate(['/auth/oauth2/authorize']);
+										} else {
+											this.externalToolsManager.externaltoolsList.updateIfLoaded();
+											// catch localStorage.redirectUri
+											if (localStorage.redirectUri) {
+												const redirectUri = new URL(localStorage.redirectUri);
+												localStorage.removeItem('redirectUri');
+												window.location.replace(redirectUri.origin);
+												return false;
+											} else {
+												// first time only do welcome
+												this.router.navigate([
+													localStorage.signup ? 'auth/welcome' : 'recipient',
+												]);
+											}
+										}
 									} else {
-										// first time only do welcome
-										this.router.navigate([ (localStorage.signup) ?'auth/welcome' :'recipient' ]);
+										this.router.navigate([
+											'signup/success',
+											{ email: profile.emails.entities[0].email },
+										]);
 									}
-								}
-							} else {
-								this.router.navigate([ 'signup/success', { email: profile.emails.entities[0].email } ]);
+								});
 							}
-
 						});
 					});
-
 				},
 				(response: HttpErrorResponse) =>
 					this.messageService.reportHandledError(
 						BadgrApiFailure.messageIfThrottableError(response.error) ||
-						"Login failed. Please check your email and password and try again."
-						, response
+							'Login failed. Please check your email and password and try again.',
+						response
 					)
 			)
-			.then(() => this.loginFinished = null);
+			.then(() => (this.loginFinished = null));
 	}
 
 	private handleQueryParamCases() {
 		try {
 			// Handle authcode exchange
-			const authCode = this.queryParams.queryStringValue("authCode", true);
+			const authCode = this.queryParams.queryStringValue('authCode', true);
 
 			// data
 			const redirectUri = localStorage.redirectUri;
 			const redirect = 'recipient';
 			if (authCode) {
-				this.sessionService.exchangeCodeForToken(authCode).then(token => {
+				this.sessionService.exchangeCodeForToken(authCode).then((token) => {
 					this.sessionService.storeToken(token);
 					this.externalToolsManager.externaltoolsList.updateIfLoaded();
-					(redirectUri) ? window.location.replace(redirectUri) : this.initFinished = this.router.navigate([ redirect ]);
+					redirectUri
+						? window.location.replace(redirectUri)
+						: (this.initFinished = this.router.navigate([redirect]));
 				});
 				return;
-			} else if (this.queryParams.queryStringValue("authToken", true)) {
+			} else if (this.queryParams.queryStringValue('authToken', true)) {
 				this.sessionService.storeToken({
-					access_token: this.queryParams.queryStringValue("authToken", true)
+					access_token: this.queryParams.queryStringValue('authToken', true),
 				});
 
 				this.externalToolsManager.externaltoolsList.updateIfLoaded();
-				(redirectUri) ? window.location.replace(redirectUri) : this.initFinished = this.router.navigate([ redirect ]);
+				redirectUri
+					? window.location.replace(redirectUri)
+					: (this.initFinished = this.router.navigate([redirect]));
 				return;
-			} else if (this.queryParams.queryStringValue("infoMessage", true)) {
-				this.messageService.reportInfoMessage(this.queryParams.queryStringValue("infoMessage", true), true);
-			} else if (this.queryParams.queryStringValue("authError", true)) {
+			} else if (this.queryParams.queryStringValue('infoMessage', true)) {
+				this.messageService.reportInfoMessage(this.queryParams.queryStringValue('infoMessage', true), true);
+			} else if (this.queryParams.queryStringValue('authError', true)) {
 				this.sessionService.logout(false);
-				this.messageService.reportHandledError(this.queryParams.queryStringValue("authError", true), null, true);
+				this.messageService.reportHandledError(
+					this.queryParams.queryStringValue('authError', true),
+					null,
+					true
+				);
 			} else if (this.sessionService.isLoggedIn) {
 				this.externalToolsManager.externaltoolsList.updateIfLoaded();
-				this.initFinished = this.router.navigate([ redirect ]);
+				this.initFinished = this.router.navigate([redirect]);
 				return;
 			}
 
 			this.initFinished = Promise.resolve(true);
 
 			// autologin, wait till get vars are processed and kick it to the end of the stack
-			if((this.sessionService.enabledExternalAuthProviders.length === 1) && (this.features.disableRegistration)) {
-				window.setTimeout(() => this.sessionService.initiateUnauthenticatedExternalAuth(this.sessionService.enabledExternalAuthProviders[0]), 0);
+			if (this.sessionService.enabledExternalAuthProviders.length === 1 && this.features.disableRegistration) {
+				window.setTimeout(
+					() =>
+						this.sessionService.initiateUnauthenticatedExternalAuth(
+							this.sessionService.enabledExternalAuthProviders[0]
+						),
+					0
+				);
 			}
-
 		} finally {
 			this.queryParams.clearInitialQueryParams();
 		}
--- src/app/common/services/user-profile-manager.service.ts
+++ src/app/common/services/user-profile-manager.service.ts
@@ -1,9 +1,10 @@
-import {forwardRef, Inject, Injectable} from '@angular/core';
-import {UserProfileApiService} from './user-profile-api.service';
-import {StandaloneEntitySet} from '../model/managed-entity-set';
-import {UserProfile} from '../model/user-profile.model';
-import {ApiUserProfile} from '../model/user-profile-api.model';
-import {CommonEntityManager} from '../../entity-manager/services/common-entity-manager.service';
+import { forwardRef, Inject, Injectable } from '@angular/core';
+import { UserProfileApiService } from './user-profile-api.service';
+import { StandaloneEntitySet } from '../model/managed-entity-set';
+import { UserProfile } from '../model/user-profile.model';
+import { ApiUserProfile } from '../model/user-profile-api.model';
+import { CommonEntityManager } from '../../entity-manager/services/common-entity-manager.service';
+import { resolve } from 'dns';
 
 /**
  * Manager for the singleton `UserProfile` instance that provides access to the current user's profile.
@@ -11,9 +12,14 @@ import {CommonEntityManager} from '../../entity-manager/services/common-entity-m
 @Injectable()
 export class UserProfileManager {
 	userProfileSet = new StandaloneEntitySet<UserProfile, ApiUserProfile>(
-		apiModel => new UserProfile(this.commonEntityManager),
-		apiModel => UserProfile.currentProfileId,
-		() => this.profileService.getProfile().then(p => [p])
+		(apiModel) => new UserProfile(this.commonEntityManager),
+		(apiModel) => UserProfile.currentProfileId,
+		() => {
+			return this.profileService
+				.getProfile()
+				.then((p) => [p])
+				.catch((e) => []);
+		}
 	);
 
 	/**
@@ -22,18 +28,40 @@ export class UserProfileManager {
 	 *
 	 * @returns {UserProfile}
 	 */
-	get userProfile() { return this.userProfileSet.entities[0]; }
+	get userProfile() {
+		return this.userProfileSet.entities[0];
+	}
 
 	/**
 	 * A promise for the loaded user profile.
 	 *
 	 * @returns {Promise<UserProfile>}
 	 */
-	get userProfilePromise(): Promise<UserProfile> { return this.userProfileSet.loadedPromise.then(() => this.userProfile); }
+	get userProfilePromise(): Promise<UserProfile> {
+		return this.userProfileSet.loadedPromise.then(() => this.userProfile);
+	}
 
 	constructor(
 		@Inject(forwardRef(() => CommonEntityManager))
 		public commonEntityManager: CommonEntityManager,
 		public profileService: UserProfileApiService
 	) {}
+
+	reloadUserProfileSet() {
+		return new Promise((resolve, reject) => {
+			this.userProfileSet = new StandaloneEntitySet<UserProfile, ApiUserProfile>(
+				(apiModel) => new UserProfile(this.commonEntityManager),
+				(apiModel) => UserProfile.currentProfileId,
+				() => {
+					return this.profileService
+						.getProfile()
+						.then((p) => [p])
+						.catch((e) => [])
+						.finally(() => {
+							resolve('userProfileSet updated');
+						});
+				}
+			);
+		});
+	}
 }
