8695bd47d9e8bb38b52d3aabc55b72becfc29594
Timber <timber@systemli.org>
2024-02-15 11:45:22 +0000

    UI create curtain (#30)
    * Basic hacky curtain
    
    * Move curtain to allow impressum
    
    Moved the curtain to `app.component.html`, to still show the footer,
    containing the impressum etc. Also don't show the curtain when the url
    contains "impressum".
    
    * Disable curtain with query parameter
    
    The curtain can be disabled with the query parameter
    `?curtainEnabled=false`. The disabled state is then stored in the local
    storage, until either the local storage is cleared, or the url is called
    with the query parameter `?curtainEnabled=true`.
    
    * Remove unneeded curtain class
    
    ---------
    
    Authored-by: Timber <timber@opensenselab.org>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,8 +45,31 @@ import { TranslateService } from '@ngx-translate/core';
 		'[class.l-stickyfooter-chromeless]': '! showAppChrome',
 	},
 	templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, AfterViewInit {
+    /**
+     * Enables or disables the "curtain" feature, hiding the normal page.
+     */
+    curtainEnabled = true;
+    get curtain() {
+        let re = /\?curtainEnabled=(\w*)/i;
+        let match = this.router.url.match(re);
+        if (match && match.length == 2) {
+            let param: string = match[1];
+            if (param == 'false' || param == 'true' ||
+                param == 'yes' || param == 'no')
+                localStorage.setItem("curtainEnabled",
+                                     param == 'true' || param == 'yes'
+                                         ? 'true' : 'false');
+        }
+
+        let local = localStorage.getItem("curtainEnabled");
+        if (local == 'false' || local == 'true')
+            this.curtainEnabled = localStorage.getItem("curtainEnabled") == 'true';
+        return this.curtainEnabled && !this.router.url.includes('impressum');
+    }
+
 	title = 'Badgr Angular';
 	loggedIn = false;
 	mobileNavOpen = false;
--- src/app/public/components/start/start.component.ts
+++ src/app/public/components/start/start.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-start',
-  templateUrl: './start.component.html',
-  styleUrls: ['./start.component.scss']
+    selector: 'app-start',
+    templateUrl: './start.component.html',
+    styleUrls: ['./start.component.scss']
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
