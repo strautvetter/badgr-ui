66c4feffc6721dd5e989f6c946a62e9dfaa12013
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2024-03-27 16:48:26 +0100

    Style/revert original style library (#105)
    * add tailwind and style frontpage using new style format
    
    * remove unused files
    
    * update blue color name to purble
    
    * revert changes to original css
    
    * finalize revertingto  original css
    
    ---------
    
    Co-authored-by: umut <u.tas@reedu.de>

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+
+import {
+  scopedPreflightStyles,
+  isolateForComponents, // there are also isolateInsideOfContainer and isolateOutsideOfContainer
+} from 'tailwindcss-scoped-preflight';
+
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: { body: ["rubik","Open Sans", 'sans-serif']},
+
+    extend: {
+      colors: {
+        purple: 'var(--color-purple)',
+        red: 'var(--color-red)'
+      },
+    },
+  },
+  plugins: [
+    scopedPreflightStyles({
+      isolationStrategy: isolateForComponents(
+        // selector string or array of selectors - the less/shorter - the better
+        [
+          '.oeb',
+        ],
+        // every strategy provides the same options (optional) to fine tune the transformation
+        {
+          // ignore: ["html", ":host", "*"], // when used, these will not be affected by the transformation
+          // remove: [":before", ":after"], // this can remove mentioned rules completely
+        },
+      ),
+    }),
+  ],
+}
+
