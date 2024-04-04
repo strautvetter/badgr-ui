a2a351682ae9a6285ce23ae60277a2f469481349
jona159 <65068389+jona159@users.noreply.github.com>
2024-04-04 13:08:49 +0200

    use some new styles for password change (#119)

--- src/app/auth/components/request-password-reset/request-password-reset.component.ts
+++ src/app/auth/components/request-password-reset/request-password-reset.component.ts
@@ -1,19 +1,22 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
-import {EmailValidator} from '../../../common/validators/email.validator';
-import {SessionService} from '../../../common/services/session.service';
-import {MessageService} from '../../../common/services/message.service';
-import {BaseRoutableComponent} from '../../../common/pages/base-routable.component';
-import {typedFormGroup} from '../../../common/util/typed-forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmailValidator } from '../../../common/validators/email.validator';
+import { SessionService } from '../../../common/services/session.service';
+import { MessageService } from '../../../common/services/message.service';
+import { BaseRoutableComponent } from '../../../common/pages/base-routable.component';
+import { typedFormGroup } from '../../../common/util/typed-forms';
 
 @Component({
 	selector: 'password-reset-request',
-	templateUrl: 'request-password-reset.component.html'
+	templateUrl: 'request-password-reset.component.html',
+	styleUrls: ['request-password-reset.component.scss'],
 })
 export class RequestPasswordResetComponent extends BaseRoutableComponent {
-	readonly requestPasswordResetForm = typedFormGroup()
-		.addControl('username', '', [Validators.required, EmailValidator.validEmail]);
+	readonly requestPasswordResetForm = typedFormGroup().addControl('username', '', [
+		Validators.required,
+		EmailValidator.validEmail,
+	]);
 
 	get prefilledEmail() {
 		return this.route.snapshot.params['email'];
@@ -24,7 +27,7 @@ export class RequestPasswordResetComponent extends BaseRoutableComponent {
 		private sessionService: SessionService,
 		private messageService: MessageService,
 		route: ActivatedRoute,
-		router: Router
+		router: Router,
 	) {
 		super(router, route);
 	}
@@ -42,28 +45,28 @@ export class RequestPasswordResetComponent extends BaseRoutableComponent {
 	}
 
 	submitResetRequest() {
-		if (! this.requestPasswordResetForm.markTreeDirtyAndValidate()) {
+		if (!this.requestPasswordResetForm.markTreeDirtyAndValidate()) {
 			return;
 		}
 
 		const email: string = this.requestPasswordResetForm.value.username;
 
 		this.sessionService.submitResetPasswordRequest(email).then(
-			response => this.router.navigate(['/auth/reset-password-sent']),
-			err => {
+			(response) => this.router.navigate(['/auth/reset-password-sent']),
+			(err) => {
 				if (err.status === 429) {
 					this.messageService.reportAndThrowError(
 						'Forgot password request limit exceeded.' +
-						' Please check your inbox for an existing message or wait to retry.',
-						err
+							' Please check your inbox for an existing message or wait to retry.',
+						err,
 					);
 				} else {
 					this.messageService.reportAndThrowError(
 						'Failed to send password reset request. Please contact support.',
-						err
+						err,
 					);
 				}
-			}
+			},
 		);
 	}
 }
