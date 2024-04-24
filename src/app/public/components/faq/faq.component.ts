f7bca1df99b07b8944e36e9eb40e70f7b2a2e244
jona159 <65068389+jona159@users.noreply.github.com>
2024-04-24 15:45:09 +0200

    Feat/faq page (#250)
    * readd study load field to badge creation and show conditionally in badge detail view
    
    * new faq page
    
    * english translations
    
    * add contact link

--- src/app/public/components/faq/faq.component.ts
+++ src/app/public/components/faq/faq.component.ts
@@ -1,12 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+type FaqItem = {
+	Q: string;
+	A: { text: string }[];
+	isActive?: boolean;
+};
 
 @Component({
 	selector: 'app-faq',
 	templateUrl: './faq.component.html',
-	styleUrls: ['./faq.component.css'],
+	styleUrls: ['./faq.component.scss'],
 })
 export class FaqComponent implements OnInit {
-	constructor() {}
+	data: FaqItem[] = [];
+	constructor(private translate: TranslateService) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.data = [
+			{
+				Q: this.translate.instant('FAQ.Q1'),
+				A: [
+					{ text: this.translate.instant('FAQ.A1.1') },
+					{ text: this.translate.instant('FAQ.A1.2') },
+					{ text: this.translate.instant('FAQ.A1.3') },
+					{ text: this.translate.instant('FAQ.A1.4') },
+					{ text: this.translate.instant('FAQ.A1.5') },
+					{ text: this.translate.instant('FAQ.A1.6') },
+				],
+			},
+			{
+				Q: this.translate.instant('FAQ.Q2'),
+				A: [
+					{ text: this.translate.instant('FAQ.A2.1') },
+					{ text: this.translate.instant('FAQ.A2.2') },
+					{ text: this.translate.instant('FAQ.A2.3') },
+					{ text: this.translate.instant('FAQ.A2.4') },
+				],
+			},
+			{
+				Q: this.translate.instant('FAQ.Q3'),
+				A: [{ text: this.translate.instant('FAQ.A3') }],
+			},
+			{
+				Q: this.translate.instant('FAQ.Q4'),
+				A: [{ text: this.translate.instant('FAQ.A4') }],
+			},
+			{
+				Q: this.translate.instant('FAQ.Q5'),
+				A: [{ text: this.translate.instant('FAQ.A5') }],
+			},
+			{
+				Q: this.translate.instant('FAQ.Q6'),
+				A: [{ text: this.translate.instant('FAQ.A6') }],
+			},
+		];
+	}
+
+	//Inspired by https://stackblitz.com/edit/angular-accordion-demo-app
+
+	toggleAccordian(event, index) {
+		const element = event.target;
+		element.classList.toggle('active');
+		if (this.data[index].isActive) {
+			this.data[index].isActive = false;
+		} else {
+			this.data[index].isActive = true;
+		}
+		const panel = element.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + 'px';
+		}
+	}
 }
