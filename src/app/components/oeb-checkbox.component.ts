0cf2ee38764a8af0c6b37bac6a148ffbcdc912fa
jona159 <65068389+jona159@users.noreply.github.com>
2024-07-17 16:34:19 +0200

    feat: redesign competencies, adjust checkbox for formcontrols (#504)
    * feat: redesign competencies, adjust checkbox for formcontrols
    
    * fix: remove comment

--- src/app/components/oeb-checkbox.component.ts
+++ src/app/components/oeb-checkbox.component.ts
@@ -1,12 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, forwardRef } from '@angular/core';
 import { HlmCheckboxComponent } from './spartan/ui-checkbox-helm/src';
 import { HlmPDirective } from './spartan/ui-typography-helm/src/lib/hlm-p.directive';
-import { FormControl } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
 	selector: 'oeb-checkbox',
 	standalone: true,
 	imports: [HlmPDirective, HlmCheckboxComponent],
+	providers: [{
+		provide: NG_VALUE_ACCESSOR,
+		useExisting: forwardRef(() => OebCheckboxComponent),
+		multi: true
+	}],
 	template: `
 		<label class="tw-flex tw-items-center" hlmP>
 			<hlm-checkbox [checked]="checked" (changed)="onChange($event)" class="tw-mr-2" />
@@ -14,13 +19,41 @@ import { FormControl } from '@angular/forms';
 		</label>
 	`,
 })
-export class OebCheckboxComponent {
+export class OebCheckboxComponent implements ControlValueAccessor {
 	@Input() text: string;
-	@Input() control: FormControl;
-	@Input() checked = false;
 	@Output() checkedChange = new EventEmitter<boolean>();
 
 	onChange(value) {
 		this.checkedChange.emit(value);
+	}
+
+	private _checked = false;
+
+	get checked() {
+		return this._checked;
+	}
+
+	set checked(value: boolean) {
+		this._checked = value;
+		this.checkedChange.emit(this._checked);
+		this.onChange(this._checked);
+		this.onTouched();
+	}
+
+	onTouched = () => {};
+
+	writeValue(value: boolean): void {
+		this.checked = value;
+	}
+
+	registerOnChange(fn: any): void {
+		this.onChange = fn;
+	}
+
+	registerOnTouched(fn: any): void {
+		this.onTouched = fn;
+	}
+
+	setDisabledState?(isDisabled: boolean): void {
 	}
 }
