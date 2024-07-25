9b11d4655ed5fad2f6efe879e987651099e13d33
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2024-07-25 09:12:36 +0200

    feat: update ui elements of request-password-reset (#527)
    * feat: update page style and translation
    
    * fix: update testBed
    
    * feat: update translation
    
    * feat: update form width
    
    * feat: adjust form-max-width

--- src/app/auth/components/request-password-reset/request-password-reset.component.spec.ts
+++ src/app/auth/components/request-password-reset/request-password-reset.component.spec.ts
@@ -12,6 +12,7 @@ import { FormBuilder } from '@angular/forms';
 import { COMMON_MOCKS_PROVIDERS_WITH_SUBS } from '../../../mocks/mocks.module.spec';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BadgrCommonModule, COMMON_IMPORTS } from '../../../common/badgr-common.module';
+import { TranslateTestingModule } from 'ngx-translate-testing';
 
 describe('RequestPasswordResetComponent', () => {
 	let fixture;
@@ -19,7 +20,7 @@ describe('RequestPasswordResetComponent', () => {
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			imports: [RouterTestingModule, BadgrCommonModule, ...COMMON_IMPORTS],
+			imports: [RouterTestingModule, BadgrCommonModule, ...COMMON_IMPORTS, TranslateTestingModule.withTranslations('de', {})],
 			declarations: [RequestPasswordResetComponent],
 			providers: [FormBuilder, ...COMMON_MOCKS_PROVIDERS_WITH_SUBS],
 			schemas: [CUSTOM_ELEMENTS_SCHEMA],
