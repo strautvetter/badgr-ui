52451f4f94ece2df2f4b01cbcd591cb85fe26dbe
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2024-10-23 15:38:27 +0200

    fix: adjust eu-logo and button width in mobile view (#765)
    * fix: adjust eu-logo and button width in mobile view
    
    * fix: update width of search-badge input
    
    * Revert "Merge branch 'main' into fix/burger-menu-is-cut-off"
    
    This reverts commit 011ae53384cbea0597d7f309d212ee843b5ca85a, reversing
    changes made to c29e6bb81e54b4b5de9ab8951aee5c7bfbfe6b44.
    
    * Revert "Revert "Merge branch 'main' into fix/burger-menu-is-cut-off""
    
    This reverts commit 0921d3d58ba0ff5303e3cf9148c6873f09764023.

--- src/app/components/spartan/ui-button-helm/src/lib/hlm-button.directive.ts
+++ src/app/components/spartan/ui-button-helm/src/lib/hlm-button.directive.ts
@@ -23,7 +23,7 @@ export const buttonVariants = cva(
 			},
 			size: {
 				default:
-					'md:tw-py-[15px] md:tw-px-[60px] md:tw-text-[20px] md:tw-leading-[30px] tw-py-[10.5px] tw-px-[42px] tw-text-[14px] tw-leading-[21px] tw-border-2 tw-font-bold',
+					'md:tw-py-[15px] sm:tw-px-[42px] md:tw-px-[60px] md:tw-text-[20px] md:tw-leading-[30px] tw-py-[10.5px] tw-px-[30px] tw-text-[14px] tw-leading-[21px] tw-border-2 tw-font-bold',
 				md: 'tw-py-[10px] tw-px-[25px] md:tw-rounded-[10px] tw-rounded-[7px] tw-text-[20px] tw-leading-[28px] tw-border tw-font-bold',
 				sm: 'tw-py-[6px] tw-px-[20px] md:tw-rounded-[10px] tw-rounded-[7px] tw-text-[16px] tw-leading-[24px] tw-border tw-font-bold',
 				xs: 'tw-py-[4px] tw-px-[16px] md:tw-rounded-[10px] tw-rounded-[7px] tw-border',
