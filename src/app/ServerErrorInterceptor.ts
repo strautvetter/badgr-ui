dec82572134f5a3ae701dd70b734264534a6fe34
Eric Thieme-Garmann <erictg96@googlemail.com>
2024-12-03 17:37:03 +0100

    fix: dont show dialog on wrong password (#890)
    * fix: dont show dialog on wrong password
    
    * fix: comments
    
    ---------
    
    Co-authored-by: Eric Thieme-Garmann <erictg96@gmail.com>

--- src/app/ServerErrorInterceptor.ts
+++ src/app/ServerErrorInterceptor.ts
@@ -7,7 +7,7 @@ import {
   HttpErrorResponse,
 } from '@angular/common/http';
 
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, throwError, of } from 'rxjs';
 import { ErrorDialogComponent } from './common/dialogs/oeb-dialogs/error-dialog.component';
 import { HlmDialogService } from './components/spartan/ui-dialog-helm/src/lib/hlm-dialog.service';
 
@@ -17,22 +17,26 @@ export class ServerErrorInterceptor implements HttpInterceptor {
 
     constructor() {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const baseUrl = this.getBaseUrl();
-    return next.handle(req).pipe(
-      catchError((error: any) => {
-        if (this.isServerError(error, baseUrl)) {
-            const dialogRef = this._hlmDialogService.open(ErrorDialogComponent, {
-                context: {
-                    error: error,
-                },
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      const baseUrl = this.getBaseUrl();
+  
+      return next.handle(req).pipe(
+        catchError((error: any) => {
+          if (this.shouldIgnoreError(error)) {
+            return of(); 
+          }
+  
+          if (this.isServerError(error, baseUrl)) {
+            this._hlmDialogService.open(ErrorDialogComponent, {
+              context: {
+                error: error,
+              },
             });
-        }
-        // Redirect error, in case other components need it
-        return throwError(() => error);
-      })
-    );
-  }
+          }
+          return throwError(() => error);
+        })
+      );
+    }
 
   private getBaseUrl(): string {
     const config = localStorage.getItem('config');
@@ -53,4 +57,18 @@ export class ServerErrorInterceptor implements HttpInterceptor {
       error.url?.startsWith(baseurl) 
     );
   }
+
+  // Helper function to decide which error should be ignored 
+  // some errors are already catched by  BadgrApiFailure class
+
+
+  private shouldIgnoreError(error: any): boolean {
+    return (
+      error instanceof HttpErrorResponse &&
+      error.status === 400 &&
+      error.error?.error === 'invalid_grant' &&
+      error.error?.error_description === 'Invalid credentials given.'
+    );
+  }
+
 }
